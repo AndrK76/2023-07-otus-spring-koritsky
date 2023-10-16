fa00ac1147478741175662c870440d66d63aed44
AndrK76 <andrk-zlt@74.ru>
2023-10-17 00:46:17 +0500

    hw10 start

---
+++ hw10-ajax/src/main/java/ru/otus/andrk/LibraryApplication.java
@@ -0,0 +1,14 @@
+package ru.otus.andrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+		System.out.println("http://localhost:8080");
+	}
+
+}
